d3afe079c37aad31a5c5b5f60a5facd8bee3f3dd
Zain-Bajwa <i191288@nu.edu.pk>
2022-08-30 15:09:27 +0500

    Writing your first Django app, part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.http import HttpResponse
-from django.shortcuts import render
 
 from .models import Question
 
@@ -12,18 +12,22 @@ def index(request):
 
 # Leave the rest of the views (detail, results, vote) unchanged
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(
+#         request, 'polls/detail.html', {
+#             'question_id': question.id,
+#             'question': question.question_text,
+#             'pub_date': question.pub_date
+#         }
+#     )
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(
-        request, 'polls/detail.html', {
-            'question_id': question.id,
-            'question': question.question_text,
-            'pub_date': question.pub_date
-        }
-    )
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
